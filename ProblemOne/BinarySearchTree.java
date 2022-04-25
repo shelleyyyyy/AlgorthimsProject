b0dadc165ccc625ff26ed8f1f7f070a09b89a5de
BoleBorson <corsonca23@mail.vmi.edu>
2022-04-24 21:30:52 -0400

    pblm 1 done

--- ProblemOne/BinarySearchTree.java
+++ ProblemOne/BinarySearchTree.java
@@ -11,19 +11,36 @@ public class BinarySearchTree<AnyType extends Comparable<AnyType>> {
         BinaryNode<AnyType> left;
         BinaryNode<AnyType> right;
 
-        // BinaryNode(AnyType element) {
-        //     HashMap<AnyType, Integer> map = new HashMap<>();
-        //     this.map = map.put(element, 0);
-        //     this.element = element;
-        //     this.left = null;
-        //     this.right = null;
-        // }
+        BinaryNode(AnyType element) {
+            HashMap<AnyType, Integer> map = new HashMap<>();
+            map.put(element, 1);
+            this.map = map;
+            this.element = element;
+            this.left = null;
+            this.right = null;
+        }
 
         BinaryNode(AnyType element, BinaryNode<AnyType> left, BinaryNode<AnyType> right) {
-            this.map.put(element, 0);
+            this.map.put(element, 1);
             this.element = element;
             this.left = left;
             this.right = right;
+        }
+
+        public AnyType getElement() {
+            return element;
+        }
+
+        public Integer getFreq() {
+            return map.get(element);
+        }
+
+        public BinaryNode<AnyType> getLeft() {
+            return left;
+        }
+
+        public BinaryNode<AnyType> getRight() {
+            return right;
         }
 
         public StringBuilder toString(StringBuilder prefix, boolean isTail, StringBuilder sb) {
@@ -76,6 +93,22 @@ public class BinarySearchTree<AnyType extends Comparable<AnyType>> {
         return contains(x, root);
     }
 
+    public Integer count(AnyType x) {
+        return count(x, root);
+    }
+
+    public boolean removeOne(AnyType x) {
+        return removeOne(x, root);
+    }
+
+    public boolean removeAll(AnyType x) {
+        if (removeAll(x, root) != null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     private BinaryNode<AnyType> findMin(BinaryNode<AnyType> root) {
 
         if (root == null)
@@ -99,19 +132,21 @@ public class BinarySearchTree<AnyType extends Comparable<AnyType>> {
 
     private BinaryNode<AnyType> insert(AnyType x, BinaryNode<AnyType> t) {
 
-        if (t == null)
+        if (t == null) {
             return new BinaryNode<>(x);
+        }
 
         int compareResult = x.compareTo(t.element);
 
-        if (compareResult < 0)
+        if (compareResult == 0) {
+            Integer value = t.map.get(x);
+            t.map.put(x, value += 1);
+        } else if (compareResult < 0)
             t.left = insert(x, t.left);
         else if (compareResult > 0)
             t.right = insert(x, t.right);
-        else if (compareResult == 0) {
-            Integer value = t.map.get(x);
-            t.map.put(x, value += 1);
-        }
+        else
+            ;
 
         return t;
     }
@@ -142,84 +177,88 @@ public class BinarySearchTree<AnyType extends Comparable<AnyType>> {
         else if (compareResult > 0)
             return count(x, t.right);
         else
-            return t.map.get(x);
+            return t.getFreq();
     }
 
-    /*
-     * private boolean removeOne(AnyType x, BinaryNode<AnyType> t) {
-     * if (t == null)
-     * return t;
-     * 
-     * int compareResult = x.compareTo(t.element);
-     * 
-     * if (compareResult < 0)
-     * t.left = remove(x, t.left);
-     * else if (compareResult > 0)
-     * t.right = remove(x, t.right);
-     * 
-     * if (t.map.get(x) == 1) {
-     * if (t.left != null && t.right != null) {
-     * t.element = findMin(t.right).element;
-     * t.right = remove(t.element, t.right);
-     * 
-     * } else
-     * t = (t.left != null) ? t.left : t.right;
-     * 
-     * return t;
-     * } else {
-     * value = t.get(x);
-     * t.map.put(x, value -= 1);
-     * }
-     * 
-     * return t;
-     * }
-     * 
-     * private BinaryNode<AnyType> removeAll(AnyType x, BinaryNode<AnyType> t) {
-     * if (t == null)
-     * return t;
-     * 
-     * int compareResult = x.compareTo(t.element);
-     * 
-     * if (compareResult < 0)
-     * t.left = remove(x, t.left);
-     * else if (compareResult > 0)
-     * t.right = remove(x, t.right);
-     * 
-     * else if (t.left != null && t.right != null) {
-     * t.element = findMin(t.right).element;
-     * t.right = remove(t.element, t.right);
-     * 
-     * } else
-     * t = (t.left != null) ? t.left : t.right;
-     * 
-     * return t;
-     * }
-     */
+    private boolean removeOne(AnyType x, BinaryNode<AnyType> t) {
+
+        if (t == null)
+            return false;
+
+        int compareResult = x.compareTo(t.element);
+
+        if (compareResult < 0)
+            return removeOne(x, t.left);
+        else if (compareResult > 0)
+            return removeOne(x, t.right);
+        else {
+            if (t.getFreq() > 1) {
+                Integer value = t.map.get(x);
+                t.map.put(x, value -= 1);
+                return true;
+            } else {
+                return removeAll(x);
+            }
+        }
+    }
+
+    private BinaryNode<AnyType> removeAll(AnyType x, BinaryNode<AnyType> t) {
+        if (t == null)
+            return t;
+
+        int compareResult = x.compareTo(t.element);
+
+        if (compareResult < 0)
+            t.left = removeAll(x, t.left);
+        else if (compareResult > 0)
+            t.right = removeAll(x, t.right);
+
+        else if (t.left != null && t.right != null) {
+            t.element = findMin(t.right).element;
+            t.right = removeAll(t.element, t.right);
+
+        } else
+            t = (t.left != null) ? t.left : t.right;
+
+        return t;
+    }
+
+    public static <AnyType> void inOrder(BinaryNode<AnyType> treeNode) {
+
+        if (treeNode.getLeft() != null) {
+            inOrder(treeNode.getLeft());
+        }
+
+        System.out.println(treeNode.getElement() + " freq: " + treeNode.getFreq());
+
+        if (treeNode.getRight() != null) {
+            inOrder(treeNode.getRight());
+        }
+    }
 
     public static void main(String[] args) {
         BinarySearchTree<Integer> bst = new BinarySearchTree<>();
 
-        for (int i = 1; i <= 20; i++) {
-            int rand = 1 + (int) (Math.random() * 5);
+        int[] array = { 5, 2, 6, 10, 3, 9, 3, 5, 5, 5 };
 
-            bst.insert(rand, bst.root);
+        for (int i = 0; i < array.length; i++) {
+            bst.insert(array[i]);
         }
 
-        /*
-         * System.out.println("Original Tree");
-         * System.out.println(bst.root.toString());
-         * 
-         * int key = 1 + (int) (Math.random() * 10);
-         * 
-         * System.out.println(key + " is in tree?" + bst.contains(key));
-         * System.out.println("Min element in the tree is " + bst.findMin());
-         * System.out.println("Max element in the tree is " + bst.findMax());
-         * 
-         * 
-         * bst.remove(key);
-         * 
-         * System.out.println("Mofified Tree remove " + key);
-         * System.out.println(bst.root.toString());
-         */
+        System.out.println("Original Tree");
+        System.out.println(bst.root.toString());
+        // inOrder(bst.root);
+        System.out.println("Key 5 has a frquency of: " + bst.count(5));
+        bst.removeOne(5);
+        System.out.println("After removeOne key 5 has a frquency of: " + bst.count(5));
+        bst.removeOne(9);
+        System.out.println("After removeOne key 9 has a frquency of: " + bst.count(9) + " *no longer in tree*");
+        bst.removeAll(5);
+        System.out.println("After removeAll key 5 has a frquency of: " + bst.count(5));
+        System.out.println("Updated Tree");
+        System.out.println(bst.root.toString());
+        System.out.println("Does 10 exist in the tree? " + bst.contains(10));
+        System.out.println("Does 9 exist in the tree? " + bst.contains(9));
+
     }
 }
